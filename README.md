d74f8682646f95a877c2f4c9ec6febeacd206971
jovnc <95868357+jovnc@users.noreply.github.com>
2025-08-16 21:14:13 +0800

    Add bias to perceptron

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # Machine Learning in C
 
-Coding machine learning from scratch in C.
+This project will implement basic machine learning algorithms from scratch in C, starting from a single perceptron.
 
-Take derivative of function to determine which direction does function grows. To find minimum, move in opposite direction of gradient.
+## Core Concepts
+
+### Artificial Neuron (Perceptron)
+
+Perceptron is the most basic unit of a neural network. It takes one or more inputs, processes them, and produces an output.
+
+Linear combination of inputs(x), weights (w), and a bias.
+
+$$ y = w_1 (x_1) + w_2 (x_2) + ... + w_n (x_n) + b $$
+
+**Gradient Descent**
+
+Find correct values for weights (w) and bias (b) using smart guesses through gradient descent.
+
+- Iterative optimization algorithm, used to minimise cost function, eg. MSE
+- How it works: take derivative of function to determine which direction does function grows. To find minimum, move in opposite direction of gradient
+- Uses finite differences to approximate gradients for simplification
+
+$$f'(x) = \frac{f(x + h) - f(x)}{h}$$
--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 #define SEED 120
 #define LEARNING_RATE 0.1f
@@ -18,20 +17,29 @@ float train[][2] = {
 #define TRAIN_COUNT (sizeof(train) / sizeof(train[0]))
 
 float rand_float(void);
-float mean_squared_error(float);
-float calculate_gradient(float, float (*)(float));
+float mean_squared_error(float, float);
+float calculate_gradient_w(float, float, float (*)(float, float));
+float calculate_gradient_b(float, float, float (*)(float, float));
 
 int main() {
     srand(SEED);
     float w = rand_float() * 10.0f;
+    float b = rand_float() * 5.0f;
 
     // start training loop
     for (int i = 0; i < 100; i++) {
-        float grad = calculate_gradient(w, mean_squared_error);
-        w -= LEARNING_RATE * grad; // update weight in opposite direction of gradient
-        printf("Loss: %f\n", mean_squared_error(w));
+        float c = mean_squared_error(w, b);
+
+        // Calculate gradients
+        float dw = (mean_squared_error(w + EPSILON, b) - c) / EPSILON;
+        float db = (mean_squared_error(w, b + EPSILON) - c) / EPSILON;
+
+        w -= LEARNING_RATE * dw;
+        b -= LEARNING_RATE * db;
+        printf("Loss: %f\n", mean_squared_error(w, b));
     }
     printf("Final weight: %f\n", w);
+    printf("Final bias: %f\n", b);
     return 0;
 }
 
@@ -46,22 +54,13 @@ float rand_float(void) {
  * Calculate the mean squared error for a given weight
  * Cost function
  */
-float mean_squared_error(float w) {
+float mean_squared_error(float w, float b) {
     float total_error = 0.0f;
     for (size_t i = 0; i < TRAIN_COUNT; i++) {
         float x = train[i][0];
-        float y = x * w;
+        float y = x * w + b;
         float error = train[i][1] - y;
         total_error += error * error;
     }
     return total_error / (float) TRAIN_COUNT;
-}
-
-/*
-* Calculate derivative (gradient) of function
-* Simplification using finite difference
-* Formula: f'(x) = (f(x + h) - f(x)) / h
-*/
-float calculate_gradient(float w, float (*function)(float)) {
-    return (function(w + EPSILON) - function(w)) / EPSILON;
 }
