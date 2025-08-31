6f37c64f6446014f0669f3b148d23d2d65c84127
jovnc <95868357+jovnc@users.noreply.github.com>
2025-09-01 00:32:10 +0800

    refactor: Add build folder for compiled code

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Compile the C programs
-clang -Wall -Wextra -o mlp mlp.c
-clang -Wall -Wextra -o gates gates.c
-clang -Wall -Wextra -o twice twice.c
+clang -Wall -Wextra -o build/mlp mlp.c
+clang -Wall -Wextra -o build/gates gates.c
+clang -Wall -Wextra -o build/twice twice.c
 
 # Run the compiled program (choose any)
-./mlp
+./build/mlp
